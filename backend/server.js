ec6ed96e54dbe340b3f60c4598c35237b35b6f58
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-05-03 20:45:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect("mongodb+srv://dasunathauda99:QYaZclvaFXKLVy49@countries-app.kmeoz0j.mongodb.net/?retryWrites=true&w=majority&appName=countries-app")
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(5000, () => {
+      console.log(`Server running on port 5000`);
     });
   })
   .catch(err => console.error(err));
