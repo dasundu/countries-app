7cccc9803a35b1538bf35428b568d0093e16e74a
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-05-03 20:39:10 +0530

    Update authService.js

--- frontend/src/service/authService.js
+++ frontend/src/service/authService.js
@@ -1,7 +1,7 @@
 // frontend/src/services/authService.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'countries-app-production-49e4.up.railway.app';
+const API_URL ='https://countries-app-production-49e4.up.railway.app/';
 
 // Register user
 export const register = async (userData) => {
